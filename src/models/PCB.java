b11f7ed7c242ff47f9bbfa174a11896882109501
SFMand <sultanfbm12@gmail.com>
2026-05-05 19:46:05 +0300

    first commit

---
+++ src/models/PCB.java
@@ -0,0 +1,89 @@
+package src.models;
+
+public class PCB {
+
+  public enum State {
+    NEW, READY, RUNNING, TERMINATED
+  }
+
+  private int processId;
+  private State state;
+  private int cpuBurstTime;
+  private int priority;
+  private int memoryRequired;
+  private int waitingTime;
+  private int turnaroundTime;
+
+  public PCB(int processId, int cpuBurstTime, int priority, int memoryRequired) {
+    this.processId = processId;
+    this.cpuBurstTime = cpuBurstTime;
+    this.priority = priority;
+    this.memoryRequired = memoryRequired;
+
+    this.state = State.NEW;
+    this.waitingTime = 0;
+    this.turnaroundTime = 0;
+  }
+
+  public int getProcessId() {
+    return processId;
+  }
+
+  public void setProcessId(int processId) {
+    this.processId = processId;
+  }
+
+  public State getState() {
+    return state;
+  }
+
+  public void setState(State state) {
+    this.state = state;
+  }
+
+  public int getCpuBurstTime() {
+    return cpuBurstTime;
+  }
+
+  public void setCpuBurstTime(int cpuBurstTime) {
+    this.cpuBurstTime = cpuBurstTime;
+  }
+
+  public int getPriority() {
+    return priority;
+  }
+
+  public void setPriority(int priority) {
+    this.priority = priority;
+  }
+
+  public int getMemoryRequired() {
+    return memoryRequired;
+  }
+
+  public void setMemoryRequired(int memoryRequired) {
+    this.memoryRequired = memoryRequired;
+  }
+
+  public int getWaitingTime() {
+    return waitingTime;
+  }
+
+  public void setWaitingTime(int waitingTime) {
+    this.waitingTime = waitingTime;
+  }
+
+  public int getTurnaroundTime() {
+    return turnaroundTime;
+  }
+
+  public void setTurnaroundTime(int turnaroundTime) {
+    this.turnaroundTime = turnaroundTime;
+  }
+
+  @Override
+  public String toString() {
+    return "PCB{ID=" + processId + ", Burst=" + cpuBurstTime +
+        ", Priority=" + priority + ", Mem=" + memoryRequired + "MB, State=" + state + "}";
+  }
+}
